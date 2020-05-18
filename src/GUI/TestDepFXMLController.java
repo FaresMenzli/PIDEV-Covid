394aaf54d60d5b693e768394e58d09881e4ea74e
FaresMenzli <Fares.elouissi@esprit.tn>
2020-05-18 01:09:36 +0100

    4th... 10 question button disabled

--- src/GUI/TestDepFXMLController.java
+++ src/GUI/TestDepFXMLController.java
@@ -4,7 +4,8 @@
  * and open the template in the editor.
  */
 package GUI;
-import com.esprit.utils.* ;
+
+import com.esprit.utils.*;
 import com.esprit.Service.ServiceQuestion;
 
 import com.esprit.utils.DataBase;
@@ -15,9 +16,11 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.event.EventType;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
@@ -32,38 +35,36 @@ public class TestDepFXMLController implements Initializable {
 
     @FXML
     private Button btnsuivant;
+Alert alert ;
 //    
-       private Connection con = DataBase.getInstance().getConnection();
+    private Connection con = DataBase.getInstance().getConnection();
     private Statement ste;
 
     /**
      * Initializes the controller class.
      */
-   
     @FXML
     private RadioButton rep1_1;
     @FXML
     private RadioButton rep1_2;
     @FXML
     private RadioButton rep1_3;
-     ToggleGroup group = new ToggleGroup();
+    ToggleGroup group = new ToggleGroup();
     @FXML
     private Label Question1;
-    
-     
-     
-     
-      
-   String Qu ;
-     
-    int score =0 ;
-     
+
+    int i = 1;
+
+    String Qu;
+
+    int score = 0;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         rep1_1.setToggleGroup(group);
         rep1_2.setToggleGroup(group);
         rep1_3.setToggleGroup(group);
-     
+
 //          try {
 //           ResultSet rs = ste.executeQuery("select * from Question where Q_id=1 ;")  ;
 //          while (rs.next()){
@@ -71,37 +72,38 @@ public class TestDepFXMLController implements Initializable {
 //       } catch (SQLException ex) {
 //           System.out.println(ex);}
         // TODO
-        
-       ServiceQuestion ser=new ServiceQuestion();
-       
-        Question1.setText(ser.afficheQuestion()); 
-        if (rep1_1.isSelected()) score +=5;
-        else if (rep1_2.isSelected())score+=3;
-        else if (rep1_3.isSelected())score+=0;
-        
-        
+        ServiceQuestion ser = new ServiceQuestion();
+
+        //Question1.setText(ser.afficheQuestion()); 
         System.out.println(score);
-       
-           
-        btnsuivant.setOnAction(e ->{
-            Question1.setText(ser.afficheQuestion2());
-            if (rep1_1.isSelected()) score +=5;
-        else if (rep1_2.isSelected())score+=3;
-        else if (rep1_3.isSelected())score+=0;
-             System.out.println(score);
-            
-             
+        //   while (i < 10) {
+        System.out.println("1i=" + i);
+        btnsuivant.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+
+                System.out.println("i=" + i);
+                Question1.setText(ser.TestafficheQ(i));
+                if (rep1_1.isSelected()) {
+                    score += 5;
+                } else if (rep1_2.isSelected()) {
+                    score += 3;
+                } else if (rep1_3.isSelected()) {
+                    score += 0;
+                }
+                 //alert = new Alert(Alert.AlertType.ERROR, "You must have a choice", buttons) ; checkpoint
+                // System.out.println(score);
+                i++;
+                if (i > 10) {
+                    btnsuivant.setDisable(true);
+                }
+            }
+
         }
-            
-            );
-        
-            
-            
-        
-        
-    
+        );
+
+        //   break ;
+        //}
     }
-    }    
-
-    
-
+}
--- src/com/esprit/Service/ServiceQuestion.java
+++ src/com/esprit/Service/ServiceQuestion.java
@@ -18,44 +18,96 @@ import java.util.logging.Logger;
  * @author MKadmin
  */
 public class ServiceQuestion {
+
     private Connection con = DataBase.getInstance().getConnection();
     private Statement ste;
-     public ServiceQuestion() {
+
+    public ServiceQuestion() {
         try {
             ste = con.createStatement();
         } catch (SQLException ex) {
             System.out.println(ex);
         }
     }
-     
-    public String  afficheQuestion() {
-        String Qu = null ;
-       try {
-           ResultSet rs = ste.executeQuery("select * from Question where Q_id=1 ;")  ;
-          while (rs.next()){
-            Qu = rs.getString(2);}
-       } catch (SQLException ex) {
-           System.out.println(ex);}
-         return Qu ;
-}
-public String  afficheQuestion2() {
-        String Qu = null ;
-       try {
-           ResultSet rs = ste.executeQuery("select * from Question where Q_id=2 ;")  ;
-          while (rs.next()){
-            Qu = rs.getString(2);}
-       } catch (SQLException ex) {
-           System.out.println(ex);}
-         return Qu ;
-}
-public String  afficheQuestion3() {
-        String Qu = null ;
-       try {
-           ResultSet rs = ste.executeQuery("select * from Question where Q_id=2 ;")  ;
-          while (rs.next()){
-            Qu = rs.getString(2);}
-       } catch (SQLException ex) {
-           System.out.println(ex);}
-         return Qu ;
-}
+
+    public String afficheQuestion() {
+        String Qu = null;
+        try {
+            ResultSet rs = ste.executeQuery("select * from Question where Q_id=1 ;");
+            while (rs.next()) {
+                Qu = rs.getString(2);
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+        return Qu;
+    }
+
+    public String afficheQuestion2() {
+        String Qu = null;
+        try {
+            ResultSet rs = ste.executeQuery("select * from Question where Q_id=2 ;");
+            while (rs.next()) {
+                Qu = rs.getString(2);
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+        return Qu;
+    }
+
+    public String afficheQuestion3() {
+        String Qu = null;
+        try {
+            ResultSet rs = ste.executeQuery("select * from Question where Q_id=2 ;");
+            while (rs.next()) {
+                Qu = rs.getString(2);
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+        return Qu;
+    }
+
+    public String TestafficheQ(int i) {
+        String question = "";
+
+        switch (i) {
+            case 1:
+                question = "Question loula";
+                break;
+            case 2:
+                question = "Question thenia";
+                break;
+            case 3:
+                question = "Question theltha";
+                break;
+            case 4:
+                question = "Question 4";
+                break;
+            case 5:
+                question = "Question 5";
+                break;
+            case 6:
+                question = "Question 6";
+                break;
+            case 7:
+                question = "Question 7";
+                break;
+            case 8:
+                question = "Question 8";
+                break;
+            case 9:
+                question = "Question 9";
+                break;
+            case 10:
+                question = "Question 10";
+                break;
+
+            default:
+
+        }
+
+        return question;
+    }
 }
